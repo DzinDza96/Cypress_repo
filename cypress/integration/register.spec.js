276bfce85655a46426acf06b10fcc28e58fee4d0
DzinDza96 <dzindza.mladji@gmail.com>
2021-10-08 00:07:21 +0200

    domaci

--- cypress/integration/register.spec.js
+++ cypress/integration/register.spec.js
@@ -5,14 +5,6 @@ describe("visit site", () => {
     it("get register",() => {
         cy.get('a[href="/register"]').click();
     })
-    // it("first name",() => {
-    //     cy.get('input[id=first-name]').type("SHredder");
-    //     cy.get('input[id=last-name]').type("splinteric");
-    //     cy.get('input[id=email]').clear().type("tmnt3@tmnt.com");
-    //     cy.get('input[id=password]').type("test12345");
-    //     cy.get('input[id=password-confirmation]').type("test12345");
-    //     cy.get('input[type="checkbox"]').check();
-    // })
     
     it("register not containing first name",() => {
         cy.get('input[id=last-name]').clear().type("splinteric");
@@ -21,6 +13,15 @@ describe("visit site", () => {
         cy.get('input[id=password-confirmation]').clear().type("test12345");
         cy.get('input[type="checkbox"]').check();
         cy.get('button[type="submit"]').click();
+    })
+            
+    it("firt name with special characters",() => {
+        cy.get('input[id=first-name]').clear().type("~_>");
+        cy.get('input[id=last-name]').clear().type("splinteric");
+        cy.get('input[id=email]').clear().type("tmnt5@tmnt.com");
+        cy.get('input[id=password]').clear().type("test12345");
+        cy.get('input[id=password-confirmation]').clear().type("test12345");
+        cy.get('input[type="checkbox"]').check();
     })
   
     it("register not containing last name",() => {
@@ -31,6 +32,15 @@ describe("visit site", () => {
         cy.get('input[id=password-confirmation]').clear().type("test12345");
         cy.get('input[type="checkbox"]').check();
         cy.get('button[type="submit"]').click();
+    })
+
+    it("last name with special characters",() => {
+        cy.get('input[id=first-name]').clear().type("SHredder");
+        cy.get('input[id=last-name]').clear().type("$+%");
+        cy.get('input[id=email]').clear().type("tmnt5@tmnt.com");
+        cy.get('input[id=password]').clear().type("test12345");
+        cy.get('input[id=password-confirmation]').clear().type("test12345");
+        cy.get('input[type="checkbox"]').check();
     })
 
     it("empty email input field ",() => {
@@ -70,6 +80,42 @@ describe("visit site", () => {
         cy.get('input[id=password-confirmation]').clear().type("test12345");
         cy.get('input[type="checkbox"]').check();
     })
+            
+    it("no content before @",() => {
+        cy.get('input[id=first-name]').clear().type("SHredder");
+        cy.get('input[id=last-name]').clear().type("splinteric");
+        cy.get('input[id=email]').clear().type("@tmnt.com");
+        cy.get('input[id=password]').clear().type("test12345");
+        cy.get('input[id=password-confirmation]').clear().type("test12345");
+        cy.get('input[type="checkbox"]').check();
+    })
+            
+    it("email input field containing only @gmail",() => {
+        cy.get('input[id=first-name]').clear().type("SHredder");
+        cy.get('input[id=last-name]').clear().type("splinteric");
+        cy.get('input[id=email]').clear().type("@gmail");
+        cy.get('input[id=password]').clear().type("test12345");
+        cy.get('input[id=password-confirmation]').clear().type("test12345");
+        cy.get('input[type="checkbox"]').check();
+    })
+
+    it("email with special characters",() => {
+        cy.get('input[id=first-name]').clear().type("SHredder");
+        cy.get('input[id=last-name]').clear().type("splinteric");
+        cy.get('input[id=email]').clear().type("@!%@^.com");
+        cy.get('input[id=password]').clear().type("test12345");
+        cy.get('input[id=password-confirmation]').clear().type("test12345");
+        cy.get('input[type="checkbox"]').check();
+    })
+            
+    it("spaces before and after email",() => {
+        cy.get('input[id=first-name]').clear().type("SHredder");
+        cy.get('input[id=last-name]').clear().type("splinteric");
+        cy.get('input[id=email]').clear().type("     tmnt5@tmnt.com     ");
+        cy.get('input[id=password]').clear().type("test12345");
+        cy.get('input[id=password-confirmation]').clear().type("test12345");
+        cy.get('input[type="checkbox"]').check();
+    })
 
     it("empty password fields",() => {
         cy.get('input[id=first-name]').clear().type("SHredder");
@@ -97,18 +143,59 @@ describe("visit site", () => {
         cy.get('input[id=password-confirmation]').clear().type("testttttttttt");
         cy.get('input[type="checkbox"]').check();
     })
+            
+    it("password containing only numbers",() => {
+        cy.get('input[id=first-name]').clear().type("SHredder");
+        cy.get('input[id=last-name]').clear().type("splinteric");
+        cy.get('input[id=email]').clear().type("tmnt5@tmnt.com");
+        cy.get('input[id=password]').clear().type("123456789");
+        cy.get('input[id=password-confirmation]').clear().type("123456789");
+        cy.get('input[type="checkbox"]').check();
+    })
         
-    it("",() => {
+    it("spaces before and after password",() => {
+        cy.get('input[id=first-name]').clear().type("SHredder");
+        cy.get('input[id=last-name]').clear().type("splinteric");
+        cy.get('input[id=email]').clear().type("     tmnt5@tmnt.com     ");
+        cy.get('input[id=password]').clear().type("test12345");
+        cy.get('input[id=password-confirmation]').clear().type("     test12345     ");
+        cy.get('input[type="checkbox"]').check();
+    })
+
+    it("password with only special characters",() => {
+        cy.get('input[id=first-name]').clear().type("SHredder");
+        cy.get('input[id=last-name]').clear().type("splinteric");
+        cy.get('input[id=email]').clear().type("tmnt5@tmnt.com");
+        cy.get('input[id=password]').clear().type("!@#$%^&*");
+        cy.get('input[id=password-confirmation]').clear().type("!@#$%^&*");
+        cy.get('input[type="checkbox"]').check();
+    })
+        
+    it("confirm password without data",() => {
         cy.get('input[id=first-name]').clear().type("SHredder");
         cy.get('input[id=last-name]').clear().type("splinteric");
         cy.get('input[id=email]').clear().type("tmnt5@tmnt.com");
         cy.get('input[id=password]').clear().type("test12345");
+        cy.get('input[id=password-confirmation]').clear();
+        cy.get('input[type="checkbox"]').check();
+    })
+            
+    it("confirm password with diffirent password",() => {
+        cy.get('input[id=first-name]').clear().type("SHredder");
+        cy.get('input[id=last-name]').clear().type("splinteric");
+        cy.get('input[id=email]').clear().type("tmnt5@tmnt.com");
+        cy.get('input[id=password]').clear().type("test12345");
+        cy.get('input[id=password-confirmation]').clear().type("12345test");
+        cy.get('input[type="checkbox"]').check();
+    })
+            
+    it("all valid data entered",() => {
+        cy.get('input[id=first-name]').clear().type("SHredder");
+        cy.get('input[id=last-name]').clear().type("splinteric");
+        cy.get('input[id=email]').clear().type("tmnt6@tmnt.com");
+        cy.get('input[id=password]').clear().type("test12345");
         cy.get('input[id=password-confirmation]').clear().type("test12345");
         cy.get('input[type="checkbox"]').check();
     })
-
-
-
-
 
 })
